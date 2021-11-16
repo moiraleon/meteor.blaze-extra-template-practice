485fe918caa685a69d80d626507facd7daded7cc
Moira Leon <moira.leon2@gmail.com>
2021-11-15 23:10:30 -0700

    creating template for inspirational cards

--- client/main.js
+++ client/main.js
@@ -3,20 +3,17 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
+InspirationalCards = new Mongo.Collection('inspoCard');
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
-
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
-});
+if (MediaElementAudioSourceNode.isClient){
+  Template.inspoCard.helpers({
+    inspoCard: [
+      {title: "motivational quote 1"}
+    ]
+  });
+}
+if(MediaElementAudioSourceNode.isServer){
+  MediaElementAudioSourceNode.startup(function(){
+    //
+  });
+}
