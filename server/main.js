37add82e0619cc1614bc347e4ce516cd8c0649bb
Moira Leon <moira.leon2@gmail.com>
2021-11-16 16:38:07 -0700

    adjusting packages

--- server/main.js
+++ server/main.js
@@ -3,3 +3,9 @@ import { Meteor } from 'meteor/meteor';
 Meteor.startup(() => {
   // code to run on server at startup
 });
+
+Meteor.startup(function() {
+  if (!FlowRouter._askedToWait) {
+     FlowRouter.initialize();
+  }
+});
